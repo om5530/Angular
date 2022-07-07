83e8e67023a84acd09c3d5b54d4b6ddccbd241e8
Omkar <omkarbaacha@foreignadmits.com>
2022-07-07 18:32:12 +0530

    Code Refactored

--- AssignmentApp/src/app/app.module.ts
+++ AssignmentApp/src/app/app.module.ts
@@ -4,15 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SingleViewComponent } from './single-view/single-view.component';
 import { HttpClientModule } from '@angular/common/http';
-import { GetProductsComponent } from './components/get-products/get-products.component';
-import { EmployeeComponent } from './components/employee/employee.component';
 import { DataService } from './services/data.service';
 import { RouterModule } from '@angular/router';
 import { AddStudentComponent } from './add-student/add-student.component';
 import { CustomerComponent } from './customer/customer.component';
 import { EditStudentComponent } from './edit-student/edit-student.component';
 import { FormsModule } from '@angular/forms';
-import { UpdateStudentComponent } from './update-student/update-student.component';
+import { EmployeeComponent } from './Extra-components/employee/employee.component';
+import { GetProductsComponent } from './Extra-components/get-products/get-products.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +21,6 @@ import { UpdateStudentComponent } from './update-student/update-student.componen
     AddStudentComponent,
     CustomerComponent,
     EditStudentComponent,
-    UpdateStudentComponent,
     
 
   ],
