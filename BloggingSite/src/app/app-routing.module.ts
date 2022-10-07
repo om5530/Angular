300ccf2f61e3977da8ebe0de4634270e0275bc3e
Omkar <omkarbacha25@gmail.com>
2022-10-07 10:08:14 +0530

    ..

--- BloggingSite/src/app/app-routing.module.ts
+++ BloggingSite/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './components/blog/blog.component';
-import { CreateBlogComponent } from './components/blog/create-blog/create-blog.component';
-import { GetSingleBlogComponent } from './components/blog/get-single-blog/get-single-blog.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
