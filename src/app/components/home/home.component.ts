723e26ddc131a8c3cd5ac7a6a51570184555e57f
Omkar <omkarbacha25@gmail.com>
2022-10-08 16:27:33 +0530

    ..

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from 'src/app/services/blog/blog.service';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
@@ -8,9 +9,10 @@ import { AuthService } from '../../services/auth/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public _auth :AuthService) { }
+  constructor(public _auth :AuthService, private service : BlogService) { }
 
   ngOnInit(): void {
+    this.service.openSnackBar('This site is under development, so you may encounter bugs', {duration: 5000})
   }
 
 }
